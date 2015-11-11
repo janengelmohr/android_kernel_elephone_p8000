799c5358f7635a68008cae93941d273e99f05148
thewisenerd <contact.twn@openmailbox.org>
2015-11-11 21:39:22 +0800

    pocket_mod: sysfs: enable: sysfs can't be world writable
    Signed-off-by: thewisenerd <thewisenerd@protonmail.com>
    Signed-off-by: Stefan Guendhoer <stefan@guendhoer.com>

--- drivers/misc/pocket_mod.c
+++ drivers/misc/pocket_mod.c
@@ -74,7 +74,7 @@ static ssize_t pocket_mod_set(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(pocket_mod_enable, 0777,
+static DEVICE_ATTR(pocket_mod_enable, (S_IWUSR|S_IRUGO),
 		pocket_mod_show, pocket_mod_set);
 
 static struct attribute *pocket_mod_attributes[] =
