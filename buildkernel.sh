45739e869ecbbeb37020876366ab487a089d68ed
Stefan Guendhoer <stefan@guendhoer.com>
2016-01-06 02:38:46 +0000

    add untracked files

---
+++ buildkernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#alternative working tc: aarch64-linux-android-4.9-kernel
+#alternative working tc: android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
+
+export ARCH=arm64
+export CONFIG_NO_ERROR_ON_MISMATCH=y
+export CROSS_COMPILE=aarch64-linux-android-
+export PATH=/home/osboxes/toolchains/aarch64-linux-android-4.9/bin:$PATH
+#export CONFIG_DEBUG_SECTION_MISMATCH=y
+
+find ./ -name '*~' -o -name '*.bak' | xargs rm;
+rm -rf ~/.ccache;
+make distclean -i -w V=0 && make mrproper -i -w V=0;
+echo "Compiling Kernel, using config file $1, this might take long, window will be closed after it is done...";
+echo "You will find the Logs of kernel compilation in build.log file.";
+rm ./.config;
+nice make -w V=0 $1 > ./build.log;
+make -w -j`grep 'processor' /proc/cpuinfo | wc -l` V=0 >> ./build.log;
+
