e5cc7258654f6a565d45fe9484824ff6aa2d61ae
Stefan Guendhoer <stefan@guendhoer.com>
2016-01-08 03:25:12 +0000

    update build script, fall back to unmodded toolchain for testing

--- buildkernel.sh
+++ buildkernel.sh
@@ -6,14 +6,14 @@
 export ARCH=arm64
 export CONFIG_NO_ERROR_ON_MISMATCH=y
 export CROSS_COMPILE=aarch64-linux-android-
-export PATH=/home/osboxes/toolchains/aarch64-linux-android-4.9_modded/bin:$PATH
+export PATH=/home/osboxes/toolchains/aarch64-linux-android-4.9_unmodded/bin:$PATH
 #export CONFIG_DEBUG_SECTION_MISMATCH=y
 
 find ./ -name '*~' -o -name '*.bak' | xargs rm;
 rm -rf ~/.ccache;
 make distclean -w V=0 && make mrproper -w V=0;
 echo "Compiling Kernel, using config file $1, this might take long, window will be closed after it is done...";
-echo "You will find the Logs of kernel compilation in build.log file.";
+echo "You will find errors and warnings of kernel compilation in build_errors_warnings.log file.";
 rm ./.config;
 nice make -w V=0 $1 2> ./build_errors_warnings.log;
 make -w -j`grep 'processor' /proc/cpuinfo | wc -l` V=0 2>> ./build_errors_warnings.log;
